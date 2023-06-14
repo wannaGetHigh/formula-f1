6cdcb9390ec689a2b19a27b4130387e4ab7c09a8
wannaGetHigh <ducquang2503@gmail.com>
2023-06-14 14:20:02 +0700

    first commit

---
+++ src/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import 'styles/index.css';
+import 'styles/global.css';
+import 'styles/icon.css';
+import App from './App';
+
+const root = ReactDOM.createRoot(
+    document.getElementById('root') as HTMLElement
+);
+
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+);
